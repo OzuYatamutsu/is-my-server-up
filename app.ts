f67dee3470904518833410c2753f5e13fca85208
Sean Collins <scollins36@gatech.edu>
2015-08-19 19:32:56 -0400

    More fleshing out of example implementation

--- app.ts
+++ app.ts
@@ -1,10 +1,36 @@
 ﻿/// <reference path="MonitoredSocket.ts" />
 import MonitoredSocket = require("./MonitoredSocket");
-import http = require('http')
+import http = require("http");
+import fs = require("fs");
+var config = require("config");
+
+var listenIp: string = config["serv"].ip;
+var listenPort: number = config["serv"].port;
+
+var responseData: string = fs.readFileSync("index.html", "utf-8");
+var monitoredSocks: Array<MonitoredSocket>;
+
+function init(): void {
+    for (var service in config["services"]) {
+        monitoredSocks.push(
+            new MonitoredSocket(service.endpoint, service.port)
+        );
+    }
+}
+
+function processResponse(): string {
+    var output: string = "";
+    for (var sock in monitoredSocks) {
+        sock.connect();
+        output += sock.isUp;
+    }
+
+    return output;
+}
+
+init();
 
 http.createServer(function (request, response) {
-    response.write("HIYA");
+    response.write(processResponse());
     response.end();
-}).listen(1337, '127.0.0.1');
-
-var testSocket = new MonitoredSocket("google.com", 80);
+}).listen(listenPort, listenIp);
