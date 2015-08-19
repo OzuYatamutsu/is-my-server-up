c66cafabb7c8814c8c64b718df580725db622b5f
Sean Collins <scollins36@gatech.edu>
2015-08-19 19:06:27 -0400

    Run this in node instead of in IIS

--- app.ts
+++ app.ts
@@ -1,10 +1,10 @@
 ﻿/// <reference path="MonitoredSocket.ts" />
 import MonitoredSocket = require("./MonitoredSocket");
+import http = require('http')
+
+http.createServer(function (request, response) {
+    response.write("HIYA");
+    response.end();
+}).listen(1337, '127.0.0.1');
 
 var testSocket = new MonitoredSocket("google.com", 80);
-
-window.onload = () => {
-    var el = document.getElementById('content');
-    testSocket.connect();
-    el.textContent = "Hello, world!";
-};
